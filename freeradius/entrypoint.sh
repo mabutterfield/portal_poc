4448c3040c547cd2a5e682bc4ebe4c6044ec3fb5
Michael Butterfield <mbutterfield@gmail.com>
2026-04-15 11:00:12 -0400

    fix radius

--- freeradius/entrypoint.sh
+++ freeradius/entrypoint.sh
@@ -5,9 +5,9 @@ SECRET="${SHARED_SECRET:-ch4ng3m3}"
 FGT_IP="${FORTIGATE_IP:-127.0.0.1}"
 
 # Substitute placeholders in config files at runtime.
-sed -i "s|%%SHARED_SECRET%%|${SECRET}|g"  /etc/freeradius/3.0/clients.conf
-sed -i "s|%%FORTIGATE_IP%%|${FGT_IP}|g"   /etc/freeradius/3.0/clients.conf
-sed -i "s|%%SHARED_SECRET%%|${SECRET}|g"  /etc/freeradius/3.0/mods-config/files/authorize
+sed -i "s|%%SHARED_SECRET%%|${SECRET}|g"  /etc/freeradius/clients.conf
+sed -i "s|%%FORTIGATE_IP%%|${FGT_IP}|g"   /etc/freeradius/clients.conf
+sed -i "s|%%SHARED_SECRET%%|${SECRET}|g"  /etc/freeradius/mods-config/files/authorize
 
 echo "[entrypoint] Configured client: ${FGT_IP}"
 
